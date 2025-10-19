0ab72a4e4726f1c1feb9fdf512d0716c28cb46be
econanringo <151092188+econanringo@users.noreply.github.com>
2025-10-19 15:45:18 +0900

    やったー

--- components/header.tsx
+++ components/header.tsx
@@ -6,6 +6,7 @@ import { doc, getDoc } from "firebase/firestore";
 import { useRouter } from "next/navigation";
 import { Button } from "./ui/button";
 import { useEffect, useState } from "react";
+import Link from "next/link";
 
 interface UserData {
   displayName: string;
@@ -45,21 +46,11 @@ export function Header() {
     <header className="w-full border-b">
       <div className="container flex h-14 max-w-screen-2xl items-center justify-between">
         <div className="flex items-center gap-2 font-semibold">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="h-6 w-6"
-          >
-            <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
-          </svg>
-          TravelShare
+          <Link href="/">
+            TravelShare
+          </Link>
         </div>
-                <nav className="flex items-center gap-4">
+        <nav className="flex items-center gap-4">
           {userData && (
             <div className="flex items-center gap-4">
               <span className="text-sm font-medium">
